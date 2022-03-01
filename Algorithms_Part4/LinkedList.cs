c405f5eba5cdecbb3551b45ec6e63290384029cb
swatitayal <31080980+swatitayal@users.noreply.github.com>
2022-02-28 23:39:46 -0800

    Problem1
    Problem1

---
+++ Algorithms_Part4/LinkedList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms_Part4
+{
+    public class LinkedList
+    {
+        public Node Head;
+        public void AddNode(int data)
+        {
+            Node new_node = new Node(data);
+            Node curr_node = Head;
+
+            if (Head == null)
+            {
+                Head = new_node;
+                Head.next = null;
+            }
+            else
+            {
+                while (curr_node.next != null)
+                {
+                    curr_node = curr_node.next;
+                }
+                curr_node.next = new_node;
+            }
+        }
+
+        public void Print()
+        {
+            Node curr_node = Head;
+
+            Console.Write("The elements of the Linked List are: ");
+            while (curr_node != null)
+            {
+                Console.Write(curr_node.data + " ");
+                curr_node = curr_node.next;
+            }
+        }
+    }
+
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
+
---
+++ Algorithms_Part4/Prob1_FindMiddleOfSLL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Algorithms_Part4
+{
+    class Prob1_FindMiddleOfSLL
+    {
+        static void Main(string[] args)
+        {
+            LinkedList myList = new LinkedList();
+            myList.AddNode(1);
+            myList.AddNode(2);
+            myList.AddNode(3);
+            myList.AddNode(4);
+            myList.AddNode(5);
+            myList.AddNode(6);
+
+            myList.Print();
+
+            FindMidNode(myList);
+        }
+
+        static void FindMidNode(LinkedList myList)
+        {
+            Node slow = myList.Head;
+            Node fast = myList.Head;
+
+            if (slow == null)
+            {
+                Console.WriteLine("\nThe List is empty!!");
+                return;
+            }
+            else
+            {
+                while (fast != null && fast.next != null)
+                {
+                    slow = slow.next;
+                    fast = fast.next.next;
+                }
+                Console.WriteLine("\nThe mid node is: " + slow.data);
+            }
+        }
+    }
+}
